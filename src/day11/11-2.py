0eeac4dc421e4cbec83dd4fc733458ef99a761c7
kruemmelbande <kai.scheufen@gmail.com>
2024-12-16 13:34:13 +0100

    day 11 (this will take ages to run)

---
+++ src/day11/11-2.py
@@ -0,0 +1,23 @@
+with open("inputs/11","r") as f:
+    inputs = list(map(int, f.readline().split()))
+
+def processNumber(num):
+    if num == 0:
+        return [1]
+    if len(str(num))%2==0:
+        return [
+            int(str(num)[:int(len(str(num))/2)]),
+            int(str(num)[int(len(str(num))/2):])
+            ]
+    return [num*2024]
+
+def simulationStep(inputs):
+    out = []
+    for i in map(processNumber,inputs):
+        for k in i:
+            out.append(k)
+    return out
+for i in range(75):
+    inputs = simulationStep(inputs)
+    print(i)
+    print(len(inputs))
